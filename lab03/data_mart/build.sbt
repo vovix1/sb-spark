f533885648c427407d7014f301568a68a3216db7
vovix1 <vladimir.kartsev@gmail.com>
2021-03-12 19:16:29 +0300

    Privet

--- lab03/data_mart/build.sbt
+++ lab03/data_mart/build.sbt
@@ -5,5 +5,7 @@ version := "0.1"
 scalaVersion := "2.11.12"
 
 libraryDependencies += "org.apache.spark" %% "spark-core" % "2.3.0"
-libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.4.7"
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.3.0"
+// https://mvnrepository.com/artifact/com.datastax.spark/spark-cassandra-connector-embedded
+libraryDependencies += "com.datastax.spark" %% "spark-cassandra-connector-embedded" % "2.3.0"
 libraryDependencies += "org.elasticsearch" %% "elasticsearch-spark-20" % "7.7.0"
--- lab03/data_mart/src/main/scala/data_mart.scala
+++ lab03/data_mart/src/main/scala/data_mart.scala
@@ -1,20 +1,34 @@
-package datamart
+//package data_mart
 
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.sql.{Row, SparkSession}
 import org.apache.spark.rdd.RDD
 
-object  Main extends App {
-  val conf = (new SparkConf(true)
-                  .setMaster("yarn")
-                  .setAppName("datamart"))
-  val spark = SparkSession.builder.config(conf).getOrCreate
+object  data_mart  {
 
-  val cities: Vector[String] = Vector("Moscow", "Paris", "Madrid", "London", "New York")
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf(true)
+      .setMaster("yarn")
+      .setAppName("data_mart")
+    val spark = SparkSession.builder.config(conf).getOrCreate
+    val sc = spark.sparkContext
+
+    val cities: Vector[String] = Vector("Moscow", "Paris", "Madrid", "London", "New York")
 
 
+    spark.read
+      .format("org.apache.spark.sql.cassandra")
+      .options(Map("spark.cassandra.connection.host" -> "10.0.0.5",
+        "spark.cassandra.connection.port" -> "9042",
+        "spark.cassandra.auth.username" -> "vladimir.kartsev",
+        "spark.cassandra.auth.password" -> "vGHn5A5f",
+        "table" -> "clients",
+        "keyspace" -> "labdata"))
 
-  val rdd = spark.parallelize(cities)
-  println(s"The RDD has ${rdd.count} elements, the first one is ${rdd.first}")
+    val rdd = sc.parallelize(cities)
+    println(s"The RDD has ${rdd.count} elements, the first one is ${rdd.first}")
+  }
+
+
 
 }
